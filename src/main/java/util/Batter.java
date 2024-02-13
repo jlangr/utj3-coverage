738eb0069a4510192dfc908011164736ae73c419
Jeff L <jeff@langrsoft.com>
2024-02-12 20:37:20 -0700

    fix highlights

--- src/main/java/util/Batter.java
+++ src/main/java/util/Batter.java
@@ -3,20 +3,16 @@ package util;
 // START:Batter
 public class Batter {
    private int strikes = 0;
-   // START_HIGHLIGHT
    private int balls = 0;
-   // END_HIGHLIGHT
 
    public void foul() {
       if (strikes < 2)
          strikes++;
    }
 
-   // START_HIGHLIGHT
    public void ball() {
       balls++;
    }
-   // END_HIGHLIGHT
 
    public void strike() {
       strikes++;
@@ -32,16 +28,12 @@ public class Batter {
    }
    // END_HIGHLIGHT
 
-   // START_HIGHLIGHT
    private boolean walked() {
       return balls == 4;
    }
-   // END_HIGHLIGHT
 
-   // START_HIGHLIGHT
    private boolean struckOut() {
       return strikes == 3;
    }
-   // END_HIGHLIGHT
 }
 // END:Batter
