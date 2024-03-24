bc224d72ca54faf60f79b2d8ccf6a22eea5f0ace
Jeff L <jeff@langrsoft.com>
2024-03-23 20:31:38 -0600

    condensed

--- src/main/java/util/Batter.java
+++ src/main/java/util/Batter.java
@@ -6,21 +6,14 @@ public class Batter {
    private int balls = 0;
 
    public void foul() {
-      if (strikes < 2)
-         strikes++;
+      if (strikes < 2) strikes++;
    }
 
-   public void ball() {
-      balls++;
-   }
+   public void ball() { balls++; }
 
-   public void strike() {
-      strikes++;
-   }
+   public void strike() { strikes++; }
 
-   public int strikes() {
-      return strikes;
-   }
+   public int strikes() { return strikes; }
 
    // START_HIGHLIGHT
    public boolean isDone() {
@@ -28,12 +21,8 @@ public class Batter {
    }
    // END_HIGHLIGHT
 
-   private boolean walked() {
-      return balls == 4;
-   }
+   private boolean walked() { return balls == 4; }
 
-   private boolean struckOut() {
-      return strikes == 3;
-   }
+   private boolean struckOut() { return strikes == 3; }
 }
 // END:Batter
